875438e9c1533d8a9701b5ae8eccc1559f395352
Ron Bachmann <laurigbach@gmail.com>
2020-09-04 10:27:10 -0300

    Implemented an example grid template for products

--- hello.py
+++ hello.py
@@ -8,9 +8,31 @@ bootstrap = Bootstrap(app)
 def index():
     return render_template('index.html')
 
-# @app.route('/user/<name>')
-# def user(name):
-#     return render_template('user.html', name=name)
+@app.route('/layout')
+def layout():
+    return render_template('layout.html')
+
+@app.route('/grid')
+def grid():
+    items = {
+    'Ruiz de Montoya': 'img/Ruiz de Montoya.jpg', 
+    'Puerto Rico': 'img/Puerto Rico.jpg', 
+    'Capiovi': 'img/Capiovi.jpg', 
+    'El alcazar': 'img/El alcazar.jpg',
+    'Ruiz de Montoya2': 'img/Ruiz de Montoya.jpg', 
+    'Puerto Rico2': 'img/Puerto Rico.jpg', 
+    'Capiovi2': 'img/Capiovi.jpg', 
+    'El alcazar2': 'img/El alcazar.jpg',
+    'Ruiz de Montoya3': 'img/Ruiz de Montoya.jpg', 
+    'Puerto Rico3': 'img/Puerto Rico.jpg', 
+    'Capiovi3': 'img/Capiovi.jpg', 
+    'El alcazar3': 'img/El alcazar.jpg',
+    'Ruiz de Montoya': 'img/Ruiz de Montoya.jpg', 
+    'Puerto Rico': 'img/Puerto Rico.jpg', 
+    'Capiovi': 'img/Capiovi.jpg', 
+    'El alcazar': 'img/El alcazar.jpg',
+    }
+    return render_template('grid.html', items=items)
 
 if __name__ == "__main__":
     app.run(debug=True)
