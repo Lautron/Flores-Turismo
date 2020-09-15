aa1043a5003a6c29bcc3362b359c6c03d189e16b
Ron Bachmann <laurigbach@gmail.com>
2020-09-15 19:21:36 -0300

    Added mock images to home page

--- flores_digital/routes.py
+++ flores_digital/routes.py
@@ -4,7 +4,13 @@ from flores_digital.models import GridTest
 
 @app.route('/')
 def index():
-    towns = {'Ruiz de Montoya': 'bg-gray', 'Puerto Rico': '', 'Capiovi': 'bg-gray', 'Garuhapé': '', 'Montecarlo': 'bg-gray', 'Caraguatay': '', 'El Alcazar': 'bg-gray'}
+    towns = {'Ruiz de Montoya': {'color': 'bg-gray', 'img': 'img/escudos/Ruiz de Montoya.jpg'}, 
+    'Puerto Rico': {'color': '', 'img': 'img/escudos/Puerto Rico.jpg'}, 
+    'Capiovi': {'color': 'bg-gray', 'img': 'img/escudos/Capiovi.jpg'}, 
+    'Garuhapé': {'color': '', 'img': 'img/escudos/Garuhapé.jpg'}, 
+    'Montecarlo': {'color': 'bg-gray', 'img': 'img/escudos/Montecarlo.jpg'}, 
+    'Caraguatay': {'color': '', 'img': 'img/escudos/Caraguatay.jpg'}, 
+    'Ruiz de Montoya': {'color': 'bg-gray', 'img': 'img/escudos/Ruiz de Montoya.jpg'}}
     return render_template('index.html', towns=towns)
 
 @app.route('/layout')
