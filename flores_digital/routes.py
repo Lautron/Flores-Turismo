45c25e5bd8a2293b181a806d0aa721f38e7e7fdc
Ron Bachmann <laurigbach@gmail.com>
2020-09-16 15:30:54 -0300

    Added icons to each town in the home page

--- flores_digital/routes.py
+++ flores_digital/routes.py
@@ -4,13 +4,15 @@ from flores_digital.models import GridTest
 
 @app.route('/')
 def index():
-    towns = {'Ruiz de Montoya': {'color': 'bg-gray', 'img': 'img/escudos/Ruiz de Montoya.jpg'}, 
-    'Puerto Rico': {'color': '', 'img': 'img/escudos/Puerto Rico.jpg'}, 
-    'Capiovi': {'color': 'bg-gray', 'img': 'img/escudos/Capiovi.jpg'}, 
-    'Garuhapé': {'color': '', 'img': 'img/escudos/Garuhapé.jpg'}, 
-    'Montecarlo': {'color': 'bg-gray', 'img': 'img/escudos/Montecarlo.jpg'}, 
-    'Caraguatay': {'color': '', 'img': 'img/escudos/Caraguatay.jpg'}, 
-    'Ruiz de Montoya': {'color': 'bg-gray', 'img': 'img/escudos/Ruiz de Montoya.jpg'}}
+    towns = {
+    'La Región de las Flores': {'img': 'img/escudos/region de las flores.png'}, 
+    'Ruiz de Montoya': {'img': 'img/escudos/Ruiz de Montoya.png'}, 
+    'Puerto Rico': {'img': 'img/escudos/Puerto Rico.png'}, 
+    'Capioví': {'img': 'img/escudos/Capiovi.png'}, 
+    'Garuhapé': {'img': 'img/escudos/Garuhapé.png'}, 
+    'Montecarlo': {'img': 'img/escudos/Montecarlo.png'}, 
+    'Caraguatay': {'img': 'img/escudos/Caraguatay.png'}, 
+    'El Alcazar': {'img': 'img/escudos/El Alcazar.png'}}
     return render_template('index.html', towns=towns)
 
 @app.route('/layout')
