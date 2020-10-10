9135e8d5ad28bb104aea271f143c4ad9d2f18a6b
Ron Bachmann <laurigbach@gmail.com>
2020-10-09 21:06:42 -0300

    Removed configs that weren't being used

--- flores_digital/__init__.py
+++ flores_digital/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '081ed310c7a5187aaaebe711c1aa0a8e'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['FLASK_DEBUG'] = 1
 
 db = SQLAlchemy(app)
 
