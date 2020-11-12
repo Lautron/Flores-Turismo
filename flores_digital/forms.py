79e28ddff45e024edb1a27182fe0716fc4035d32
Lautron <laurigbach@gmail.com>
2020-11-12 15:33:13 -0300

    created file for wtf_forms

---
+++ flores_digital/forms.py
@@ -0,0 +1,46 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, SubmitField, TextAreaField
+from flask_wtf.file import FileField, FileRequired
+from wtforms.validators import DataRequired
+
+class ProductForm(FlaskForm):
+    #Name string
+    name = StringField('Nombre', validators=[DataRequired()])
+    # Img file
+    img = FileField() # validators=[FileRequired()]
+    # Description textarea
+    description = TextAreaField('Descripción')
+    # Town select
+    town = SelectField('Localidad', choices=[
+        ('capiovi','Capioví'),
+        ('caraguatay', 'Caraguatay'),
+        ('el alcazar', 'El Alcazar'),
+        ('garuhape', 'Garuhapé'),
+        ('montecarlo', 'Montecarlo'),
+        ('puerto rico', 'Puerto Rico'),
+        ('ruiz de montoya', 'Ruiz de Montoya'),
+    ])
+    # type select
+    ptype = SelectField('Tipo de producto', choices=[
+        ('actividad','Actividad'),
+        ('alojamiento','Alojamiento'),
+        ('atraccion','Atracción'),
+        ('gastronomia','Gastronomía'),
+    ])
+    # Location string
+    location = StringField('Ubicación')
+    # phone
+    phone = StringField('Telefono')
+    # facebook
+    facebook = StringField('Facebook')
+    # email
+    email = StringField('eMail')
+    # instagram
+    instagram = StringField('Instagram')
+    # website
+    website = StringField('Pagina Web')
+    # submit
+    submit = SubmitField()
+
+
+
