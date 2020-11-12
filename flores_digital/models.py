00628d03ac9227c3800586d66bc1860f971137ef
Lautron <laurigbach@gmail.com>
2020-11-12 17:52:59 -0300

    Refactored product_type to ptype, and removed old testing code

--- flores_digital/models.py
+++ flores_digital/models.py
@@ -7,7 +7,7 @@ class ProductData(db.Model):
     img = db.Column(db.String(30), default='img/default.jpg')
     description = db.Column(db.Text, nullable=False)
     town = db.Column(db.String(30))
-    product_type = db.Column(db.String(30))
+    ptype = db.Column(db.String(30))
 
     location = db.Column(db.String(50), nullable=False)
     phone = db.Column(db.String(50))
@@ -17,13 +17,7 @@ class ProductData(db.Model):
     website = db.Column(db.String(50))
 
 if __name__ == "__main__":
-
-    test = ProductData({'name': 'Ruiz de Montoya Spa', 'description': 'This is a test', 'town': 'Ruiz de Montoya', 'product_type': 'atraction'},
-            Contact({'location': 'Ruiz de Montoya', 'phone': '1244556', 'facebook': 'Ruiz de Montoya', 'email': 'ruizdemontoya@gob.ar', 
-            'instagram': '@ruizdemontoyaturismo', 'website': 'ruizdemontoya.com.ar'}))
-    test2 = ProductData({'name': 'Ruiz de Montoya Salto', 'description': 'This is a test', 'town': 'Ruiz de Montoya', 'product_type': 'atraction'},
-            Contact({'location': 'Ruiz de Montoya', 'phone': '1244556', 'facebook': 'Ruiz de Montoya', 'email': 'ruizdemontoya@gob.ar', 
-            'instagram': '@ruizdemontoyaturismo', 'website': 'ruizdemontoya.com.ar'}))
+    pass
 
     
 
