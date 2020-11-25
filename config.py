8831e29937d8400b7a53b3dbbf3b613519be1591
Lautron <laurigbach@gmail.com>
2020-11-25 20:13:07 -0300

    Created files for heroku config

---
+++ config.py
@@ -0,0 +1,28 @@
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = '2e0ebf077566348a4601cb3138e5eb3d'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
