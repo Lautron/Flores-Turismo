285a808e425e2e6fa8402031031dd02463ad3772
Lautron <laurigbach@gmail.com>
2020-11-25 20:00:36 -0300

    Changed app.config

--- flores_digital/__init__.py
+++ flores_digital/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '081ed310c7a5187aaaebe711c1aa0a8e'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['FLASK_DEBUG'] = 1
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
