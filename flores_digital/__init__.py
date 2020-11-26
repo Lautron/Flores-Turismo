5112711f48f853a05392f4c850542792d164a2f0
Lautron <laurigbach@gmail.com>
2020-11-26 10:29:51 -0300

    Added support for .env file

--- flores_digital/__init__.py
+++ flores_digital/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
