beeef61928cfd354ac1d1438981a60340f2b5b89
Lautaro Bachmann <laurigbach@gmail.com>
2020-11-29 08:30:17 -0300

    Created file to change img file name

---
+++ flores_digital/static/product_pics/change_name.py
@@ -0,0 +1,13 @@
+import os, shutil
+
+for folderName, subfolders, filename in os.walk(os.getcwd()):
+    for file in filename:
+        if ' ' not in file and file.islower():
+            continue
+        newFileName = os.path.abspath(file.replace(' ', '-').lower())
+
+        # newFileName = os.path.abspath(folderName + '/' + newFileName)
+        # file = os.path.abspath(folderName + '/' + file)
+
+        print(f'{file} was renamed to {newFileName}')
+        os.rename(os.path.abspath(file), newFileName)
